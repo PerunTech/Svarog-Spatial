78375fa9bfd7ec4533600be06fc0cb030fee0b6f
Igi <i.dimov@prtech.mk>
2019-09-05 14:33:54 +0200

    added src and dist added linting, rudimentary added .gitignore

---
+++ src/Spatial.js
@@ -0,0 +1,8 @@
+(function () {
+    S = {
+        version: '0.0.1',
+        test_clue: 'run test page [@ ./dist/test.html] find me in global window.S via browser console'
+    }
+
+    window.S = S
+})()
