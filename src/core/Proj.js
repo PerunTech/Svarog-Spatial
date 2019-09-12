8072b85e4d9f59834fa2373b7e74b31b6d173193
Igi <i.dimov@prtech.mk>
2019-09-12 09:15:59 +0200

    dev CRS => added fields, specced init logic added scale and zoom
    runnin instance test on main

--- src/core/Proj.js
+++ src/core/Proj.js
@@ -6,6 +6,11 @@ import {IProj} from '../interface/IProj'
 
 export const Proj = (function () {
 
+    /*
+    let _setScales = function (opt) {
+
+    }
+*/
     let Projection = Core.extend({
         // constructor function
         init: function(code, def, bounds) {
@@ -44,12 +49,65 @@ export const Proj = (function () {
 
                 Util.setOptions(this, opt);
 
-                // handle origin, scales, resolutions, infinite flag
-            }
+                if (this.options.origin) {
+                    this.transformation = IRender.transformation(
+                        1, -this.options.origin[0],
+                        -1, this.options.origin[1]);
+                }
 
-            // scale, zoom,
+                if (this.options.scales) {
+                    this._scales = this.options.scales;
+                } else if (this.options.resolutions) {
+                    this._scales = [];
+
+                    for (let i = this.options.resolutions.length - 1; i >= 0; i--) {
+                        if (this.options.resolutions[i]) {
+                            this._scales[i] = 1 / this.options.resolutions[i];
+                        }
+                    }
+                }
+
+                this.infinite = !this.options.bounds;
+            },
+
+            scale: function (zoom) {
+                let iZoom = Math.floor(zoom),
+                    baseScale,
+                    nextScale,
+                    scaleDiff,
+                    zDiff;
+
+                if (zoom === iZoom) {
+                    return this._scales[zoom];
+                } else {
+                    baseScale = this._scales[iZoom];
+                    nextScale = this._scales[iZoom + 1];
+                    scaleDiff = nextScale - baseScale;
+                    zDiff = zoom - iZoom;
+
+                    return baseScale + scaleDiff * zDiff;
+                }
+            },
+
+            zoom: function (scale) {
+                let downScale = this._closestElement(this._scales, scale),
+                    downZoom = this._scales.indexOf(downScale),
+                    nextScale,
+                    nextZoom;
+
+                    // Check if scale is downScale => return array index
+                if (scale === downScale) { return downZoom; }
+                if (downScale === undefined) { return -Infinity; }
+
+                // Interpolate
+                nextZoom = downZoom + 1;
+                nextScale = this._scales[nextZoom];
+                if (nextScale === undefined) { return Infinity; }
+
+                return (scale - downScale) / (nextScale - downScale) + downZoom;
+            },
+
+            _closestElement: function () {}
         })
-
-
     }
 })()
