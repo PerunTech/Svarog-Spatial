5ce6639f8228e55df79119b8b164701df788d844
Igi <i.dimov@prtech.mk>
2019-09-13 10:12:44 +0200

    refactored crs (no more CRS.crs accessors) Class is top, only factory exposed for import tested zoom and scale overrides, provided explanations to methods where absent `private` methods exposed in CRS instance, underscore annotation
    Need to implement generation of resolutions, consider dpi to ppx to real world distance
    extend factory for the above
    refactor _scales setter, need to solve for scales vs resolutions priority

--- src/Spatial.js
+++ src/Spatial.js
@@ -12,8 +12,8 @@ export {Core} from './core/Core'
 export {IRender} from './interface/IRender'
 export {Proj} from './core/proj/Proj'
 
-import {CRS} from './core/proj/CRS'
-let test_crs = CRS.crs('EPSG:32638',
+import {crs} from './core/proj/CRS'
+let test_crs = crs('EPSG:32638',
 '+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext +no_defs',
 {
     origin: [-180, +90],
--- src/core/proj/CRS.js
+++ src/core/proj/CRS.js
@@ -3,31 +3,156 @@ import {Util} from 'leaflet'
 import {Proj} from './Proj'
 import {IRender} from '../../interface/IRender'
 
-export const CRS = (function () {
-    // Get the closest lowest element in an array.
-    // `#revise_me`, may move to private scope in root CRS or Util if use is found.
-    let _closestElement = function (arr, el) {
-        let nLow;
-		for (let i = arr.length; i--;) {
-			if (arr[i] <= el && (nLow === undefined || nLow < arr[i])) {
-				nLow = arr[i];
-			}
-        }
+/**
+ * @class CRS
+ * @extends Core
+ * @implements iCRS
+ */
+let CRS = Core.extend({
+    // merge custom crs implementation,
+    // methods required in fundamental map and assets operations
+    includes: IRender.crs(),
+    //config
+    options: {
+        transformation: IRender.transformation(1, 0, -1, 0)
+    },
+    /**
+     * Mean Earth Radius = 6371000 m, as recommended for use by
+     * the International Union of Geodesy and Geophysics.
+     *
+     * The Earth radius R vraies from 6356.752 km at the poles to 6378.137 km at the equator.
+     * Perhaps this number can be tweaked based on mean latitude of the project country,
+     * in order to improve accuracy.
+     */
+    R: 6371000, // in meters [m]
 
-		return nLow;
-    }
+    // constructor
+    init: function (code, def, opt = {}) {
+        this.projection = Proj.projection(code, def, opt.bounds)
+        this.code = code;
+
+        Util.setOptions(this, opt);
+
+        this.transformation = this._setTransformation(this.options);
+        this._scales = this._setScales(this.options);
+        this.infinite = !this.options.bounds;
+    },
+
+    /**
+     * @function scale
+     * (zoom: Number): Number
+     * @override iCRS.scale
+     *
+     * Returns the scale used when transforming projected coordinates
+     * into pixel coordinates for a particular zoom.
+     *
+     * The original iCRS implementation uses `256 * 2^zoom` for Mercator-based CRS.
+     *
+     * @param {Number} zoom
+     *
+     * @returns Scale number value
+     */
+    scale: function (zoom) {
+        let iZoom = Math.floor(zoom),
+            baseScale,
+            nextScale,
+            scaleDiff,
+            zDiff;
+
+        if (zoom === iZoom) {
+            return this._scales[zoom];
+        } else {
+            baseScale = this._scales[iZoom];
+            nextScale = this._scales[iZoom + 1];
+            scaleDiff = nextScale - baseScale;
+            zDiff = zoom - iZoom;
+
+            return baseScale + scaleDiff * zDiff;
+        }
+    },
+
+    /**
+     * @function zoom
+     * (scale: Number): Number
+     * @override iCRS.zoom
+     *
+     * Inverse of `this.scale`. Calculates the zoom level corresponding to a scale factor of `scale`.
+     *
+     * The original iCRS implementaion uses `Math.log(scale / 256) / Math.LN2` for calculation.
+     *
+     * @param {Number} scale
+     *
+     * @returns Zoom number value
+     */
+    zoom: function (scale) {
+        let downScale = this._closestElement(this._scales, scale),
+            downZoom = this._scales.indexOf(downScale),
+            nextScale,
+            nextZoom;
+
+        // Check if scale is downScale => return array index
+        if (scale === downScale) { return downZoom; }
+        if (downScale === undefined) { return -Infinity; }
+        // Interpolate
+        nextZoom = downZoom + 1;
+        nextScale = this._scales[nextZoom];
+        if (nextScale === undefined) { return Infinity; }
+
+        return (scale - downScale) / (nextScale - downScale) + downZoom;
+    },
+
+    /**
+     * @function distance
+     * (latlng1: LatLng, latlng2: LatLng): distance [m] {Number}
+     *
+     * Uses `Haversine` formula to calculate the distance between two geographical points.
+     * Calculates great circle distance on an assumed sphere, which the Earth is not.
+     *
+     * Mean error appromixation is 0.5% and the calculation is best on small distances, less than 5km,
+     * which is what we need.
+     * It is far better than the spherical law of cosine approximation, due to use of sine,
+     * while the latter uses cosine which approaches 0.9999~ on very small distances and may result in
+     * large errors due to rounding. (JS engine does 15 digits though, currently)
+     *
+     * Its use is mostly visual rather than technical, still if the accuracy is insufficient
+     * we should consider Vincenty' formula (accurate to 0.1mm)
+     * (this is a very fine site in general)
+     * https://www.movable-type.co.uk/scripts/latlong-vincenty.html
+     * `#revise_me`
+     *
+     * @param {LatLng} latlng1
+     * @param {LatLng} latlng2
+     *
+     * @return distance in meters
+     */
+    distance: function (latlng1, latlng2) {
+        let rad = Math.PI / 180;
+        // convert latitude degrees to radians for easier trigonometry
+        let phi1 = latlng1.lat * rad,
+            phi2 = latlng2.lat * rad;
+        // sine of latitude difference, in radians
+        let delta_phi = Math.sin((latlng2.lat - latlng1.lat) * rad / 2);
+        //sine of longitutde difference, in radiance
+        let delta_lambda = Math.sin((latlng2.lng - latlng1.lng) * rad / 2);
+        // square of half the chord length between pA and pB
+        let a = delta_phi * delta_phi + Math.cos(phi1) * Math.cos(phi2) * delta_lambda * delta_lambda;
+        // andgular distance, in radiance
+        let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt( 1- a));
+
+        return this.R * c;  // distance in meters
+    },
 
     // `#revise_me`, may move to crs instance scope and provide runtime accessor.
-    let _setTransformation = function (opt) {
+    _setTransformation: function (opt) {
         return opt.origin
             ? IRender.transformation(1, -opt.origin[0], -1, opt.origin[1])
             : opt.transformation;
-    }
+    },
 
     // `#revise_me`, may move to crs instance scope and provide runtime accessor.
     // access may create more problems than solutions,
     // why change scales definition in middle of operation?
-    let _setScales = function (opt) {
+    _setScales: function (opt) {
         let scales = [];
         // exit if config has scales
         if (opt.scales) { return opt.scales; }
@@ -41,142 +166,38 @@ export const CRS = (function () {
         }
 
         return scales;
-    }
+    },
 
-    /**
-     * @class CRS
-     * @extends Core
-     * @implements iCRS
-     */
-    let CRS = Core.extend({
-        // merge custom crs implementation,
-        // methods required in fundamental map and assets operations
-        includes: IRender.crs(),
-        //config
-        options: {
-            transformation: IRender.transformation(1, 0, -1, 0)
-        },
-        /**
-         * Mean Earth Radius = 6371000 m, as recommended for use by
-         * the International Union of Geodesy and Geophysics.
-         *
-         * The Earth radius R vraies from 6356.752 km at the poles to 6378.137 km at the equator.
-         * Perhaps this number can be tweaked based on mean latitude of the project country,
-         * in order to improve accuracy.
-         */
-        R: 6371000, // in meters [m]
-
-        // constructor
-        // mandatory fields below
-        init: function (code, def, opt = {}) {
-            this.projection = Proj.projection(code, def, opt.bounds)
-            this.code = code;
-
-            Util.setOptions(this, opt);
-
-            this.transformation = _setTransformation(this.options);
-            this._scales = _setScales(this.options);
-            this.infinite = !this.options.bounds;
-        },
-
-        scale: function (zoom) {
-            let iZoom = Math.floor(zoom),
-                baseScale,
-                nextScale,
-                scaleDiff,
-                zDiff;
-
-            if (zoom === iZoom) {
-                return this._scales[zoom];
-            } else {
-                baseScale = this._scales[iZoom];
-                nextScale = this._scales[iZoom + 1];
-                scaleDiff = nextScale - baseScale;
-                zDiff = zoom - iZoom;
-
-                return baseScale + scaleDiff * zDiff;
-            }
-        },
-
-        zoom: function (scale) {
-            let downScale = _closestElement(this._scales, scale),
-                downZoom = this._scales.indexOf(downScale),
-                nextScale,
-                nextZoom;
-
-                // Check if scale is downScale => return array index
-            if (scale === downScale) { return downZoom; }
-            if (downScale === undefined) { return -Infinity; }
-
-            // Interpolate
-            nextZoom = downZoom + 1;
-            nextScale = this._scales[nextZoom];
-            if (nextScale === undefined) { return Infinity; }
-
-            return (scale - downScale) / (nextScale - downScale) + downZoom;
-        },
-
-        /**
-         * @function distance
-         * (latlng1: LatLng, latlng2: LatLng): distance [m] {Number}
-         *
-         * Uses `Haversine` formula to calculate the distance between two geographical points.
-         * Calculates great circle distance on an assumed sphere, which the Earth is not.
-         *
-         * Mean error appromixation is 0.5% and the calculation is best on small distances, less than 5km,
-         * which is what we need.
-         * It is far better than the spherical law of cosine approximation, due to use of sine,
-         * while the latter uses cosine which approaches 0.9999~ on very small distances and may result in
-         * large errors due to rounding. (JS engine does 15 digits though, currently)
-         *
-         * Its use is mostly visual rather than technical, still if the accuracy is insufficient
-         * we should consider Vincenty' formula (accurate to 0.1mm)
-         * (this is a very fine site in general)
-         * https://www.movable-type.co.uk/scripts/latlong-vincenty.html
-         * `#revise_me`
-         *
-         * @param {LatLng} latlng1
-         * @param {LatLng} latlng2
-         *
-         * @return distance in meters
-         */
-        distance: function (latlng1, latlng2) {
-            let rad = Math.PI / 180;
-            // convert latitude degrees to radians for easier trigonometry
-            let phi1 = latlng1.lat * rad,
-                phi2 = latlng2.lat * rad;
-
-            // sine of latitude difference, in radians
-            let delta_phi = Math.sin((latlng2.lat - latlng1.lat) * rad / 2);
-            //sine of longitutde difference, in radiance
-            let delta_lambda = Math.sin((latlng2.lng - latlng1.lng) * rad / 2);
-
-            // square of half the chord length between pA and pB
-            let a = delta_phi * delta_phi + Math.cos(phi1) * Math.cos(phi2) * delta_lambda * delta_lambda;
-            // andgular distance, in radiance
-            let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt( 1- a));
-
-            return this.R * c;  // distance in meters
-        },
-    })
-
-    return {
-        /**
-         * @factory crs
-         *
-         * @param {String} code
-         *        CRS code, as specified by the European Petroleum Survey Group.
-         *        (e.g. EPSG: 4326)
-         * @param {String} def
-         *        Proj4 defintion of the projection.
-         *        (e.g. '+proj=utm +zone=38 +ellps=WGS84 +datum=WGS84 +units=m +no_defs')
-         * @param {Object} opt
-         *        Configuration object
-         *
-         * @return CRS
-         */
-        crs: function (code, def, opt = {}) {
-            return new CRS(code, def, opt);
+    // Get the closest lowest element in an array.
+    // `#revise_me`, may move to private scope in root CRS or Util if use is found.
+    _closestElement: function (arr, el) {
+        let nLow;
+		for (let i = arr.length; i--;) {
+			if (arr[i] <= el && (nLow === undefined || nLow < arr[i])) {
+				nLow = arr[i];
+			}
         }
+
+		return nLow;
     }
-})()
+})
+
+/**
+ * @factory crs
+ *
+ * @param {String} code
+ *        CRS code, as specified by the European Petroleum Survey Group.
+ *        (e.g. EPSG: 4326)
+ * @param {String} def
+ *        Proj4 defintion of the projection.
+ *        (e.g. '+proj=utm +zone=38 +ellps=WGS84 +datum=WGS84 +units=m +no_defs')
+ * @param {Object} opt
+ *        Configuration object
+ *
+ * @return CRS
+ */
+export const crs = function (code, def, opt = {}) {
+    // implement factory
+    // add resolution generation per config, consider monitor dpi
+    return new CRS(code, def, opt);
+}
