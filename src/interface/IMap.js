d9d3049cecc48ab04acea134c1019e20d3f32a56
Igi <i.dimov@prtech.mk>
2019-09-27 10:07:59 +0200

    remove notes

--- src/interface/IMap.js
+++ src/interface/IMap.js
@@ -2,16 +2,7 @@ import { Interface } from "../core/Interface";
 import { Util } from '../core/Util';
 import { Map } from 'leaflet'
 
-
 /**
  * @interface iMap
  */
 export const iMap = Interface.define(Map.prototype, Util.create(Map.prototype))
-
-
-/*
-getCenter() {
-    console.log('shadowed implementation prototype, do curry-pass next');
-    return _mapProto.getCenter();
-}
-*/
