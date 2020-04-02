0ec573553c5456337fd72aa367fec470d41b8071
Igi <i.dimov@prtech.mk>
2020-04-02 15:47:02 +0200

    Added WSSpatial.java

---
+++ backend/src/main/java/com/prtech/perun_spatial/service/WSSpatial.java
@@ -0,0 +1,22 @@
+package com.prtech.perun_spatial.service;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+
+import org.apache.logging.log4j.Logger;
+
+import com.prtech.svarog.SvConf;
+
+public class WSSpatial {
+	static final Logger log4j = SvConf.getLogger(WSSpatial.class);
+	
+	@GET
+	@Path("/origin")
+	@Produces("text/html;charset=utf-8")
+	public Response getHoldingLocation() {
+		// Prilep
+		return Response.status(200).entity("41.32779361376413,21.501059532165527,41.34355071838928,21.54028415679932").build();
+	};
+}
