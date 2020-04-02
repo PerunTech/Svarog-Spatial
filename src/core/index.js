ea0af0242e0985bb8bc40caa616ad3b4bc4878e0
Igi <i.dimov@prtech.mk>
2020-04-02 12:30:58 +0200

    Added index to core, export inlcude resources. Conclude core merge.

---
+++ src/core/index.js
@@ -0,0 +1,28 @@
+// Utilities
+export { util } from './utils/Util';
+
+// Service
+export { http } from './service/HTTP';
+export { factory } from './service/Factory';
+
+// Abstract
+// export { Class } from './abstract/Class';
+// export { Interface } from './abstract/Interface';
+
+// Model
+export { connect } from './model/Connect';
+export { Provider } from './model/Provider';
+export { thunk } from './model/Thunk';
+export { store } from './model/Store';
+export { state } from './model/State';
+
+// Proj
+export { crs } from './projection/CRS';
+export { projection } from './projection/Projection';
+export { geoJson } from './projection/GeoJSON';
+
+// Map
+export { Map } from './map/Map';
+export { control } from './map/Control';
+export { renderCycle } from './map/RenderCycle';
+export { MapContainer } from './map/Container';
