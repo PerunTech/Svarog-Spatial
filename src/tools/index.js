54c3fe0199876bc03bfc7e81181ab45f9c9848ba
Igi <i.dimov@prtech.mk>
2020-04-02 12:38:48 +0200

    Added index.js to tools namespace, export include content. Conclude tools merge.

---
+++ src/tools/index.js
@@ -0,0 +1,17 @@
+// Coordinate
+export { coordinate } from './coordinate/Coordinate';
+export { limits } from './coordinate/Limits';
+
+// Draw
+export { draw } from './draw/Draw';
+
+// Edit
+export { edit } from './edit/Edit';
+
+// Measurement
+export { measureMarker } from './measurement/Marker';
+export { measureLine } from './measurement/Line';
+export { measureCircle } from './measurement/Circle';
+
+// Navigation
+export { navigation } from './navigation/Navigation';
