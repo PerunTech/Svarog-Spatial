0508b96241ce22fa98acbfff71765d5adee2eb2f
Igi <i.dimov@prtech.mk>
2020-04-02 12:37:47 +0200

    Added tools/measurement namespace, merge content.

---
+++ src/tools/measurement/Circle.js
@@ -0,0 +1,87 @@
+import { util, factory } from '../../core';
+import { circleArea, addInitHook, formatArea } from './Util';
+
+const { layerGroup, marker, Circle } = factory;
+
+export const measureCircle = {
+    showMeasurements: function (options) {
+        if (!this._map || this._measurementLayer) return this;
+
+        this._measurementOptions = util.obj.assignDeep({
+            showOnHover: false,
+            showArea: true,
+            lang: {
+                totalArea: 'Total area',
+            }
+        }, options || {});
+
+        this._measurementLayer = layerGroup().addTo(this._map);
+        this.updateMeasurements();
+
+        this._map.on('zoomend', this.updateMeasurements, this);
+
+        return this;
+    },
+
+    hideMeasurements: function () {
+        if (!this._map) return this;
+
+        this._map.on('zoomend', this.updateMeasurements, this);
+
+        if (!this._measurementLayer) return this;
+        this._map.removeLayer(this._measurementLayer);
+        this._measurementLayer = null;
+
+        return this;
+    },
+
+    updateMeasurements: function () {
+        if (!this._measurementLayer) return;
+
+        let latLng = this.getLatLng(),
+            options = this._measurementOptions,
+            formatter = options.formatArea || util.fn.bind(this.formatArea, this);
+
+        this._measurementLayer.clearLayers();
+
+        if (options.showArea) {
+            formatter = options.formatArea || util.fn.bind(this.formatArea, this);
+            let area = circleArea(this.getRadius());
+            
+            marker.measurement(latLng, formatter(area), options.lang.totalArea, 0, options)
+                .addTo(this._measurementLayer);
+        }
+    },
+
+    onAdd: util.fn.override(Circle.prototype.onAdd, function (protoVal) {
+        let showOnHover = this.options.measurementOptions && this.options.measurementOptions.showOnHover;
+        if (this.options.showMeasurements && !showOnHover) {
+            this.showMeasurements(this.options.measurementOptions);
+        }
+
+        return protoVal;
+    }),
+
+    onRemove: util.fn.override(Circle.prototype.onRemove, function (protoVal) {
+        this.hideMeasurements();
+        return protoVal;
+    }, true),
+
+    setLatLng: util.fn.override(Circle.prototype.setLatLng, function (protoVal) {
+        this.updateMeasurements();
+        return protoVal;
+    }),
+
+    setRadius: util.fn.override(Circle.prototype.setRadius, function (protoVal) {
+        this.updateMeasurements();
+        return protoVal;
+    }),
+
+    formatArea: formatArea,
+};
+
+Circle.include(measureCircle);
+
+Circle.addInitHook(function () {
+    addInitHook.call(this);
+});
---
+++ src/tools/measurement/Line.js
@@ -0,0 +1,140 @@
+import { util, factory } from '../../core';
+import { addInitHook, formatArea, formatDistance, ringArea } from './Util';
+
+const { marker, layerGroup, Polyline, Polygon } = factory;
+
+export const measureLine = {
+    showMeasurements: function (options) {
+        if (!this._map || this._measurementLayer) return this;
+
+        this._measurementOptions = util.obj.assignDeep({
+            showOnHover: (options && options.showOnHover) || false,
+            minPixelDistance: 30,
+            showDistances: true,
+            showArea: true,
+            lang: {
+                totalLength: 'Total length',
+                totalArea: 'Total area',
+                segmentLength: 'Segment length'
+            }
+        }, options || {});
+
+        this._measurementLayer = layerGroup().addTo(this._map);
+        this.updateMeasurements();
+
+        this._map.on('zoomend', this.updateMeasurements, this);
+
+        return this;
+    },
+
+    hideMeasurements: function () {
+        if (!this._map) return this;
+
+        this._map.off('zoomend', this.updateMeasurements, this);
+
+        if (!this._measurementLayer) return this;
+        this._map.removeLayer(this._measurementLayer);
+        this._measurementLayer = null;
+
+        return this;
+    },
+
+    updateMeasurements: function () {
+        if (!this._measurementLayer) return this;
+
+        let formatter, ll1, ll2, p1, p2, pixelDist, dist,
+            latLngs = this.getLatLngs(),
+            isPolygon = this instanceof Polygon,
+            options = this._measurementOptions,
+            totalDist = 0;
+
+        // Outer ring is stored as an array in the first element, use that instead.
+        if (latLngs && latLngs.length && util.arr.isArray(latLngs[0])) {
+            latLngs = latLngs[0];
+        }
+
+        this._measurementLayer.clearLayers();
+
+        if (this._measurementOptions.showDistances && latLngs.length > 1) {
+            formatter = this._measurementOptions.formatDistance || util.fn.bind(this.formatDistance, this);
+
+            for (let i = 1, len = latLngs.length; (isPolygon && i <= len) || i < len; i++) {
+                ll1 = latLngs[i - 1];
+                ll2 = latLngs[i % len];
+                dist = ll1.distanceTo(ll2);
+                totalDist += dist;
+
+                p1 = this._map.latLngToLayerPoint(ll1);
+                p2 = this._map.latLngToLayerPoint(ll2);
+
+                pixelDist = p1.distanceTo(p2);
+
+                if (pixelDist >= options.minPixelDistance) {
+                    marker.measurement(
+                        this._map.layerPointToLatLng([(p1.x + p2.x) / 2, (p1.y + p2.y) / 2]),
+                        formatter(dist),
+                        options.lang.segmentLength,
+                        this._getRotation(ll1, ll2),
+                        options)
+                            .addTo(this._measurementLayer);
+                }
+            }
+
+            // Show total length for polylines
+            if (!isPolygon) {
+                marker.measurement(ll2, formatter(totalDist), options.lang.totalLength, 0, options)
+                    .addTo(this._measurementLayer);
+            }
+        }
+
+        if (isPolygon && options.showArea && latLngs.length > 2) {
+            formatter = options.formatArea || util.fn.bind(this.formatArea, this);
+            let area = ringArea(latLngs);
+            
+            marker.measurement(this.getBounds().getCenter(), formatter(area), options.lang.totalArea, 0, options)
+                .addTo(this._measurementLayer);
+        }
+
+        return this;
+    },
+
+    onAdd: util.fn.override(Polyline.prototype.onAdd, function (protoVal) {
+        let showOnHover = this.options.measurementOptions && this.options.measurementOptions.showOnHover;
+        if (this.options.showMeasurements && !showOnHover) {
+            this.showMeasurements(this.options.measurementOptions);
+        }
+
+        return protoVal;
+    }),
+
+    onRemove: util.fn.override(Polyline.prototype.onRemove, function (protoVal) {
+        this.hideMeasurements();
+        return protoVal;
+    }, true),
+
+    setLatLngs: util.fn.override(Polyline.prototype.setLatLngs, function (protoVal) {
+        this.updateMeasurements();
+        return protoVal;
+    }),
+
+    spliceLatLngs: util.fn.override(Polyline.prototype.spliceLatLngs, function (protoVal) {
+        this.updateMeasurements();
+        return protoVal;
+    }),
+
+    formatDistance: formatDistance,
+    formatArea: formatArea,
+
+    _getRotation: function (ll1, ll2) {
+        let p1 = this._map.project(ll1),
+            p2 = this._map.project(ll2);
+
+        return Math.atan((p2.y - p1.y) / (p2.x - p1.x));
+    }
+};
+
+Polyline.include(measureLine);
+
+Polyline.addInitHook(function () {
+    addInitHook.call(this);
+});
