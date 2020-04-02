8218dfbdc576b22a4ffbd64ed1b9293d2d08a522
Igi <i.dimov@prtech.mk>
2020-04-02 13:55:13 +0200

    Format webpack

--- webpack.config.js
+++ webpack.config.js
@@ -14,48 +14,49 @@ module.exports = (mode, {env}) => {
         devServer: {
             contentBase: './backend/www',
         },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)?$/, 
-        exclude: /(node_modules)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env', '@babel/preset-react'],
-            cacheDirectory: true
-          }
+        module: {
+            rules: [
+                {
+                    test: /\.(js|jsx)?$/, 
+                    exclude: /(node_modules)/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env', '@babel/preset-react'],
+                            cacheDirectory: true
+                        }
+                    }
+                },
+                {
+                    // For pure CSS (without CSS modules)
+                    test: /\.css$/i,
+                    exclude: /\.module\.css$/i,
+                    use: ['style-loader', 'css-loader'],
+                },
+                {
+                    // For CSS modules
+                    test: /\.module\.css$/i,
+                    use: [
+                        'style-loader',
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                modules: true,
+                            },
+                        },
+                    ],
+                },
+                {
+                    test: /\.(png|jpe?g|gif|svg|eot|ttf|woff|woff2)$/i,
+                    loader: 'url-loader',
+                    options: {
+                        limit: 10000,
+                    }
+                },
+            ]
+        },
+        resolve: {
+            extensions: ['.js', '.jsx']
         }
-      },
-      {
-        // For pure CSS (without CSS modules)
-        test: /\.css$/i,
-        exclude: /\.module\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        // For CSS modules
-        test: /\.module\.css$/i,
-        use: [
-          'style-loader',
-          {
-            loader: 'css-loader',
-            options: {
-              modules: true,
-            },
-          },
-        ],
-      },
-      {
-        test: /\.(png|jpe?g|gif|svg|eot|ttf|woff|woff2)$/i,
-        loader: 'url-loader',
-        options: {
-          limit: 10000,
-        }
-      },
-    ]
-  },
-  resolve: {
-    extensions: ['.js', '.jsx']
-  }
-};
+    }
+}
