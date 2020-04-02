d53f56468a46756898edb64fd4387b9f1352e4f0
Igi <i.dimov@prtech.mk>
2020-04-02 12:37:23 +0200

    Added src/tools/edit/Edit.js

---
+++ src/tools/edit/Edit.js
@@ -0,0 +1,17 @@
+import { factory } from '../../core';
+
+export const edit = {
+    marker () { console.log(factory) },
+
+    line () { console.log(factory.PM) },
+
+    polygon () { },
+
+    rectangle () { },
+
+    circle () { },
+
+    circleMarker () { },
+
+    layer () { }
+}
