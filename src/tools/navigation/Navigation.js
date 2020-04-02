7a5f9c604572f50b99682a05b0ee79db845f3867
Igi <i.dimov@prtech.mk>
2020-04-02 12:38:31 +0200

    Added tools/navigation/Navigation.js

---
+++ src/tools/navigation/Navigation.js
@@ -0,0 +1,26 @@
+import { store, Map, factory } from '../../core';
+import { draw } from '..';
+import { NAVIGATE_VIEW } from '../../config';
+
+export const navigation = {
+    origin () {
+        return Map.fitBounds(
+            factory.boundingBox(
+                store.getState().map.origin)
+        );
+    },
+
+    point () {
+        console.log('go to location')
+    },
+
+    boxZoom () {
+        Map.on('pm:create', function fn ({layer}) {
+            Map.fitBounds(layer.getBounds()).off('pm:create', fn).removeLayer(layer);
+        });
+
+        return draw.rectangle(NAVIGATE_VIEW);
+    },
+
+    search () {}
+}
