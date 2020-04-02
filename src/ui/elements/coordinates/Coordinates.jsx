2e8862fa71bc6db4c3aa01be58670e1c3f8aac15
Igi <i.dimov@prtech.mk>
2020-04-02 12:41:42 +0200

    Added src/ui/elements/namespace, merge content pt1. Added elements:  - anchor,  - button  - coordinates

---
+++ src/ui/elements/coordinates/Coordinates.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { coordinate as coordUtil, limits } from '../../../tools';
+import { Input, Button } from '../..';
+
+export function Coordinates ({coordinates, onChange, ...props}) {
+    const { projected, labeled, validated, onBlur, ...inputProps } = props;
+
+    const isValid = (coordinate, idx) => !(coordinate.length > 3) 
+        ? {}
+        : limits.isBounded(coordinate, idx, projected)
+            ? { isValid: true }
+            : { isInvalid: true };
+
+    return Object.values(coordinates).map((coordinate, idx) => {
+        return <React.Fragment key={idx} >
+            {labeled && <Button disabled className='as-label'>{idx === 0 ? 'X:' : 'Y:'}</Button>}
+            <div className='coordinate' onBlur={onBlur} >
+                <Input {...inputProps}
+                    id={String(idx)}
+                    value={coordinate}
+                    placeholder={limits.getRange(idx, true)}
+                    onChange={onChange} 
+                    {...(validated && isValid(coordinate, idx))} />
+                <div className='invalid-feedback' >{limits.getRange(idx, projected)}</div>
+            </div>
+        </React.Fragment>
+    })
+}
+
+Coordinates.defaultProps = {
+    projected: true,
+    labeled: false,
+    validated: true
+};
