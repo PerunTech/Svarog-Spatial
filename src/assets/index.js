0909cc93f27abbbb1a4a8ae702ecf35cd6619ea2
Igi <i.dimov@prtech.mk>
2020-04-02 12:20:35 +0200

    Added index.js to src/assets/, export include all resources. Conclude assets merge.

---
+++ src/assets/index.js
@@ -0,0 +1,34 @@
+/* css.typography */
+import './css/typography/typography.css';
+import './css/typography/popup.css';
+import './css/typography/tooltip.css';
+import './css/typography/polygon.css';
+import './css/typography/measurement.css';
+
+/* css.factory */
+import './css/factory/factory.css';
+import './css/factory/icon.css';
+import './css/factory/cursor.css';
+import './css/factory/interaction.css';
+
+/* css.elements */
+import './css/elements/button.css';
+import './css/elements/input.css';
+import './css/elements/divider.css'
+
+/* css.control */
+import './css/control/control.css';
+import './css/control/zoom.css';
+import './css/control/attribution.css';
+import './css/control/layers.css';
+import './css/control/toolbar.css';
+import './css/control/status.css'
+
+/* css.tools */
+import './css/tools/draw.css';
+
+/* css.modules */
+import './css/modules/ellipser.css';
+import './css/modules/scale.css';
+
+export { registry } from './svg/Registry';
