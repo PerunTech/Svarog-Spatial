d3d7f4476778fc27933665eb855083f4cc4330c1
Igi <i.dimov@prtech.mk>
2020-04-02 13:20:24 +0200

    Added modules loading and scale.

---
+++ src/ui/modules/loading/Ellipser.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+/**
+ * Renders rotating elipses and a message.
+ * To be used as a loading widget while the application is busy.
+ * 
+ * &nbsp;
+ * 
+ * @function Ellipser (message?: string, ellipseCount?: number): JSX.Element
+ * 
+ * @param {Object} [props] - Properties.
+ * @param {string} [props.message] - The message to be shown.
+ * @param {number} [props.ellipseCount] - The number of ellipses to be rendered (max 10).
+ * 
+ * @returns JSX.Element;
+ */
+export function Ellipser ({message='', ellipseCount=7}) {
+    return <div id='ellipser' className='ellipser' >
+      <div id='globe' className='globe'>
+        {[...Array(ellipseCount)].map((v, i) => { return <div key={'e'+i} className='ellipse' /> })}
+      </div>
+      {message}
+    </div> 
+}
+
+Ellipser.propTypes = {
+  message: PropTypes.string,
+  ellipseCount: PropTypes.number
+}
---
+++ src/ui/modules/scale/ScaleControl.jsx
@@ -0,0 +1,42 @@
+import React, { useMemo } from 'react';
+import PropTypes from 'prop-types';
+import { connect, Map } from '../../../core';
+import { DropdownButton, Dropdown } from '../..';
+
+/**
+ * Generate content body of ScaleControl. Static, use map init range of scales, these will not extend
+ * for the lifetime of the application (i.e. this is the maximum extent of values, they may decrease only).
+ * 
+ * Access of crs.options.distances may fail if the currently used crs is not created with distances, 
+ * but rather with resolutions or scales directly. The crs class needs to be revised,
+ * before any assumptions are made here. 
+ */
+function _ScaleControl ({currZoom, minZoom, maxZoom}) {
+    const distances = useMemo(() => 
+        [...Map.getCRS().options.distances].splice(minZoom, maxZoom), [minZoom, maxZoom]);
+
+    return <DropdownButton title={`1 : ${distances[currZoom]}`} drop='up' alignRight >
+        {distances.map((dist, i) => {
+            return <Dropdown.Item 
+                key={i} 
+                eventKey={i} 
+                active={i === currZoom} 
+                onClick={() => Map.setView(Map.getCenter(), i)} >
+                    {`1 : ${dist}`}
+                </Dropdown.Item>})}
+    </DropdownButton>
+}
+
+_ScaleControl.propTypes = {
+    currZoom: PropTypes.number,
+    minZoom: PropTypes.number,
+    maxZoom: PropTypes.number
+}
+
+export const ScaleControl = connect(state => { 
+    return {
+        currZoom: state.map.zoom,
+        minZoom: state.map.minZoom,
+        maxZoom: state.map.maxZoom
+    }
+})(_ScaleControl);
