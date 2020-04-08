75cbdbd898c657fa0ec81ab51690d69a9a95fcfd
Igi <i.dimov@prtech.mk>
2020-04-08 15:09:41 +0200

    Remove window self-register, already configured by webpack to be packaged and exported by universal module definition.
    Remove default export.

--- frontend/Spatial.js
+++ frontend/Spatial.js
@@ -9,7 +9,7 @@ import * as tools from './tools';
 import * as ui from './ui';
 
 // Assemble plugin.
-const spatial = core.util.obj.assemble(
+export const spatial = core.util.obj.assemble(
 { // Set instance properties.
     name: name,
     version: version,
@@ -23,7 +23,3 @@ const spatial = core.util.obj.assemble(
     tools,
     ui,
 });
-
-// Register on global scope. Export default.
-window[name] = spatial;
-export default spatial;
