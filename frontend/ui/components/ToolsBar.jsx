bef4640e12e323b53095bf3ef2ae3d4f17e5163e
Igi <i.dimov@prtech.mk>
2020-05-06 10:49:58 +0200

    Further revised Navigation.jsx, removed not used markup, custom helper elements pertaining to visualization. Wrapped ToolsBar.jsx in  a div, added testing left right margins to wrapper.

--- frontend/ui/components/ToolsBar.jsx
+++ frontend/ui/components/ToolsBar.jsx
@@ -1,12 +1,14 @@
 import { React } from 'perun-core';
 import { control } from '../../core';
-import { Tab, Tabs, Digitization, Measurement, Navigation } from '..';
+import { Tab, Tabs, Digitization, Measurement, Navigation, Button } from '..';
 
 export const ToolsBar = props => 
-    <Tabs id='toolsbar' defaultActiveKey='digitization'>
-        <Tab eventKey='navigation' title='Навигација' children={<Navigation />} />
-        <Tab eventKey='digitization' title='Дигитизација' children={<Digitization />} />
-        <Tab eventKey='measurement' title='Мерења' children={<Measurement />} />
-    </Tabs>;
+    <div style={{marginLeft: '250px', marginRight: '250px'}}>
+        <Tabs id='toolsbar' defaultActiveKey='digitization' >
+            <Tab eventKey='navigation' title='Навигација' children={<Navigation />} />
+            <Tab eventKey='digitization' title='Дигитизација' children={<Digitization />} />
+            <Tab eventKey='measurement' title='Мерења' children={<Measurement />} />
+        </Tabs>
+    </div>;
 
 control(ToolsBar, {}, {position: 'top'});
