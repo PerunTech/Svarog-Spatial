424a1e3097ada4ef6f1d50fccae64f6eb76dee84
Igi <i.dimov@prtech.mk>
2020-05-08 15:29:08 +0200

    Revise drawActions.jsx, wip Revise Dialog.jsx header.

--- frontend/ui/modules/measurement/Dialog.jsx
+++ frontend/ui/modules/measurement/Dialog.jsx
@@ -1,15 +1,22 @@
-import { React } from 'perun-core';
+import { React, PropTypes } from 'perun-core';
 import { Modal } from '../..';
 import { Button } from '../../elements/button/Button';
 
-export const Dialog = () => 
+export const Dialog = ({title}) => 
     <Modal show
         backdrop={false}
         enforceFocus={false}
         container={document.getElementsByClassName('control-map')[0]}
         dialogClassName='measure-dialog' >
-            <Modal.Title>Test Measure Utility</Modal.Title>
+            <Modal.Title>
+                <Button disabled className='as-label' >{title}</Button>
+                {}
+            </Modal.Title>
             <Modal.Body >
                 <Button onClick ={() => console.log('clicked')} >Test Measure Utility Body</Button>
             </Modal.Body>
     </Modal>;
+
+Dialog.propTypes = {
+    title: PropTypes.string
+}
