a263162ad4e807a3156e14ef158c1ec5b3ca135a
Igi <i.dimov@prtech.mk>
2020-05-11 10:51:27 +0200

    Moved drawActions.jsx from toolbar to util. Revised impl, decouple from handler, pass callables directly for:  - finish shape  - undo  - cancel draw

---
+++ frontend/ui/utils/drawActions.jsx
@@ -0,0 +1,21 @@
+import { React, PropTypes } from 'perun-core';
+import { ButtonGroup, Button, Icon } from '..';
+
+export const DrawActions = ({finish, undo, cancel})  =>
+    <ButtonGroup id='draw-actions' >
+        <Button id='finish-shape' onClick={e => finish(e)} >
+            <Icon name='finish-shape' />
+        </Button>
+        <Button id='delete-last-vertex' onClick={(e) => undo(e) } >
+            <Icon name='delete-last-vertex' />
+        </Button>
+        <Button id='cancel-draw' onClick={(e) => cancel(e) } >
+            <Icon name='cancel-draw' />
+        </Button>
+    </ButtonGroup>
+
+DrawActions.propTypes = {
+    finish: PropTypes.func,
+    undo: PropTypes.func,
+    cancel: PropTypes.func
+}
