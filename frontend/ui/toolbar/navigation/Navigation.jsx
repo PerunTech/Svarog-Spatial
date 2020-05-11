3d894334af83e4466a80a4b2c5c35671201448ae
Igi <i.dimov@prtech.mk>
2020-05-11 09:15:31 +0200

    Fixed Navigation.jsx, Digitization.jsx for Process Button calls.

--- frontend/ui/toolbar/navigation/Navigation.jsx
+++ frontend/ui/toolbar/navigation/Navigation.jsx
@@ -2,14 +2,14 @@ import { React } from 'perun-core';
 import { PROCESS_ENUM } from '../../../config';
 import { Map } from '../../../core';
 import { navigation } from '../../../tools';
-import { ButtonGroup, ProcessButton } from '../..';
+import { ButtonGroup, ToolbarButton } from '../..';
 
 export const Navigation = props => 
     <ButtonGroup id='navigation' >
-        <ProcessButton id='zoom-in' onClick={() => Map.zoomIn()} />
-        <ProcessButton id='zoom-out' onClick={() => Map.zoomOut()} />
-        <ProcessButton id='search' onClick={navigation.search} />
-        <ProcessButton id='origin' onClick={navigation.origin} />
-        <ProcessButton id='location' onClick={navigation.point} />
-        <ProcessButton id={PROCESS_ENUM.view} onClick={navigation.boxZoom} />
+        <ToolbarButton id='zoom-in' onClick={() => Map.zoomIn()} />
+        <ToolbarButton id='zoom-out' onClick={() => Map.zoomOut()} />
+        <ToolbarButton id='search' onClick={navigation.search} />
+        <ToolbarButton id='origin' onClick={navigation.origin} />
+        <ToolbarButton id='location' onClick={navigation.point} />
+        <ToolbarButton id={PROCESS_ENUM.view} onClick={navigation.boxZoom} />
     </ButtonGroup>
