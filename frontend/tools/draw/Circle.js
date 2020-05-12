9d9f59c88b152c49a292a2c9ab367bb14e1c6220
Igi <i.dimov@prtech.mk>
2020-05-12 10:42:02 +0200

    Added fields in Circle.js, inherit snap, base shape.

--- frontend/tools/draw/Circle.js
+++ frontend/tools/draw/Circle.js
@@ -1,1 +1,9 @@
-export const circle = {};
+import { util, Map, factory } from '../../core';
+import { snap } from '..';
+
+export const circle = {
+    ...snap,
+    shape: 'circle',
+    options: {},
+    enabled: false,
+};
