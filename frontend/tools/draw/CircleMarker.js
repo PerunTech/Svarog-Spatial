b55342791d2d4b150a12f59cdc41266e93d36cc4
Igi <i.dimov@prtech.mk>
2020-05-12 09:35:47 +0200

    Added enable method to CircleMarker.js

--- frontend/tools/draw/CircleMarker.js
+++ frontend/tools/draw/CircleMarker.js
@@ -1,4 +1,4 @@
-import { Map } from '../../core';
+import { util, factory, Map } from '../../core';
 import { marker } from './Marker';
 
 export const circleMarker = {
@@ -6,4 +6,47 @@ export const circleMarker = {
     shape: 'circleMarker',
     options: {},
     enabled: false,
+
+    enable(options) {
+        util.assign(this.options, options);
+    
+        // change enabled state
+        this.enabled = true;
+    
+        // create a marker on click on the map
+        Map.on('click', this._createMarker, this);
+    
+        // this is the hintmarker on the mouse cursor
+        this._hintMarker = factory.circleMarker([0, 0], this.options.templineStyle);
+        this._hintMarker._pmTempLayer = true;
+        this._hintMarker.addTo(Map);
+    
+        // add tooltip to hintmarker
+        this.options.tooltips && this._hintMarker
+            .bindTooltip(getTranslation('tooltips.placeCircleMarker'), {
+                permanent: true,
+                offset: factory.point(0, 10),
+                direction: 'bottom',
+                
+                opacity: 0.8,
+            })
+            .openTooltip();
+    
+        // this is just to keep the snappable mixin happy
+        this._layer = this._hintMarker;
+    
+        // sync hint marker with mouse cursor
+        Map.on('mousemove', this._syncHintMarker, this);
+    
+        // fire drawstart event
+        Map.fire('pm:drawstart', {
+            shape: this.shape,
+            workingLayer: this._layer,
+        });
+    
+        // enable edit mode for existing markers
+        // This iteration is unacceptable, `#revise_me`
+        Map.eachLayer(layer => 
+            this.isRelevantMarker(layer) && layer.pm.enable());
+    },
 };
