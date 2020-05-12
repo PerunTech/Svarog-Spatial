901fe6d8e9be54ebc1ae3f9e36590a2d78af42d9
Igi <i.dimov@prtech.mk>
2020-05-12 15:32:25 +0200

    Fixed Circle.js wrong field emitted on drawend event. Fixed index exports of Angle.jsx.
    Reworked Angle.jsx
     - react component, combined markup and logic.
    
    Fixed measurement imports and calls, render tree.

---
+++ frontend/ui/modules/measurement/Angle.jsx
@@ -0,0 +1,50 @@
+import { React, PropTypes } from 'perun-core';
+import { util, factory, Map, connect } from "../../../core";
+import { MEASURE_LINE, PROCESS_ENUM, getProcessTitle } from '../../../config';
+import { draw } from '../../../tools';
+import { Button, Icon } from '../..';
+
+/* The internal id of the process */
+const _id = PROCESS_ENUM.angle;
+/* Layer group of all Angle measurements, register is accessible from outside. */
+export const angleMeasurements = factory.layerGroup().addTo(Map);
+
+function _Angle ({options, ...props}) {
+    const { activeId, dispatch, ..._props } = props;
+
+    /* Disables measurement handler. Called automatically on map event,
+        fired when the drawn shape is finished. */
+    const disable = React.useCallback((e) => {
+        (e && e.layer) 
+            && (angleMeasurements.addLayer(e.layer),
+                Map.fitBounds(e.layer.getBounds()).off('pm:create', disable));
+
+        dispatch({activeId: ''});
+        draw.circle.disable();
+    }, [dispatch])
+
+    /* Enables measurement handler */
+    const enable = React.useCallback(() => {
+        dispatch({ activeId: _id });
+        Map.on('pm:create', disable);
+        draw.circle.enable(util.assign(MEASURE_LINE, options));
+    }, [options, dispatch, disable])
+
+    return <Button {..._props}
+        id={_id} 
+        className={activeId === _id ? 'active' : ''}
+        onClick={() => enable() } >
+            <Icon name={_id} size='28px' />
+            <span style={{display: 'block', marginTop: '5px'}}>{getProcessTitle(_id)}</span>
+    </Button>
+}
+
+_Angle.propTypes = {
+    options: PropTypes.object,
+    activeId: PropTypes.string,
+    dispatch: PropTypes.func
+}
+
+export const Angle = connect(({process}) => { 
+    return { activeId: process.activeId };
+})(_Angle);
