1e0b367df36653ea36d3394c89c6c5acd219ca5f
Igi <i.dimov@prtech.mk>
2020-05-12 08:45:21 +0200

    Convert Marker.js to a simple object, IIFE not necessary.

--- frontend/tools/draw/Marker.js
+++ frontend/tools/draw/Marker.js
@@ -1,51 +1,49 @@
 import { util, Map, factory } from '../../core';
 
-export const marker = (function () {
-    return {
-        shape: 'marker',
-        options: {},
-        enabled: false,
+export const marker = {
+    shape: 'marker',
+    options: {},
+    enabled: false,
 
-        enable(options) {
-            util.assign(this.options, options);
+    enable(options) {
+        util.assign(this.options, options);
 
-            // change enabled state
-            this.enabled = true;
-        
-            // create a marker on click on the map
-            Map.on('click', this._createMarker, this);
-        
-            // this is the hintmarker on the mouse cursor
-            this._hintMarker = factory.marker([0, 0], this.options.markerStyle);
-            this._hintMarker._pmTempLayer = true;
-            this._hintMarker.addTo(Map);
-        
-            // add tooltip to hintmarker
-            this.options.tooltips && this._hintMarker
-                .bindTooltip(getTranslation('tooltips.placeMarker'), {
-                    permanent: true,
-                    offset: factory.point(0, 10),
-                    direction: 'bottom',
-                    
-                    opacity: 0.8,
-                })
-                .openTooltip();
+        // change enabled state
+        this.enabled = true;
+    
+        // create a marker on click on the map
+        Map.on('click', this._createMarker, this);
+    
+        // this is the hintmarker on the mouse cursor
+        this._hintMarker = factory.marker([0, 0], this.options.markerStyle);
+        this._hintMarker._pmTempLayer = true;
+        this._hintMarker.addTo(Map);
+    
+        // add tooltip to hintmarker
+        this.options.tooltips && this._hintMarker
+            .bindTooltip(getTranslation('tooltips.placeMarker'), {
+                permanent: true,
+                offset: factory.point(0, 10),
+                direction: 'bottom',
+                
+                opacity: 0.8,
+            })
+            .openTooltip();
 
-            // this is just to keep the snappable mixin happy
-            this._layer = this._hintMarker;
-        
-            // sync hint marker with mouse cursor
-            Map.on('mousemove', this._syncHintMarker, this);
-        
-            // fire drawstart event
-            Map.fire('pm:drawstart', {
-                shape: this._shape,
-                workingLayer: this._layer,
-            });
-        
-            // enable edit mode for existing markers
-            Map.eachLayer(layer => 
-                this.isRelevantMarker(layer) && layer.pm.enable());
-        },
-    }
-})()
+        // this is just to keep the snappable mixin happy
+        this._layer = this._hintMarker;
+    
+        // sync hint marker with mouse cursor
+        Map.on('mousemove', this._syncHintMarker, this);
+    
+        // fire drawstart event
+        Map.fire('pm:drawstart', {
+            shape: this._shape,
+            workingLayer: this._layer,
+        });
+    
+        // enable edit mode for existing markers
+        Map.eachLayer(layer => 
+            this.isRelevantMarker(layer) && layer.pm.enable());
+    },
+}
