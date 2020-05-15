0bd9064afacc8a8d12ac89d267c78459b6927d85
Igi <i.dimov@prtech.mk>
2020-05-15 16:05:58 +0200

    wip Length.jsx

--- frontend/ui/modules/measurement/Length.jsx
+++ frontend/ui/modules/measurement/Length.jsx
@@ -1,36 +1,40 @@
 import { React, PropTypes } from 'perun-core';
-import { factory, Map, connect, store } from "../../../core";
+import { factory, Map, connect, store, util } from "../../../core";
 import { MEASURE_LINE, PROCESS_ENUM, getProcessTitle } from '../../../config';
 import { draw } from '../../../tools';
 import { Button, Icon, Modal, DrawActions } from '../..';
 
 /* The internal id of the process */
 const _id = PROCESS_ENUM.length;
+let _sum = 0
 
 /* Layer group of all length measurements, register is accessible from outside. */
 export const lengthMeasurements = factory.layerGroup().addTo(Map);
 
-/* Registers the drawn shape. Executes when measurement shape is finished. */
-const _finishMeasurement = e => (e && e.layer) && lengthMeasurements.addLayer(e.layer);
 
-/* Disables measurement handler. Called automatically on map event,
-    fired when the drawn shape is finished. */
+/* Registers the drawn shape. Executes when measurement shape is finished. */
+const _finishMeasurement = e => 
+    (e && e.layer) && (lengthMeasurements.addLayer(e.layer), _sum = _sum + calcCurrentLength(store.getState().measurement.totalLength));
+
+const calcCurrentLength = str => Number(Array.from(str).filter(s =>
+    '.0123456789'.split('').includes(s)).join('')) * (str.includes('km') ? 1000 : 1);
+
+/* Disables measurement handler. */
 const _disable = () => {
+    _sum = 0
     Map.off('new_shape', _finishMeasurement);
-    store.dispatch({activeId: ''});
-    draw.line._finishShape();
+    store.dispatch({activeId: '', totalLength: '0 m'});
+    draw.line.disable('force');
 }; 
 
 /* Enables measurement handler */
 const _enable = () => {
-    Map.on('new_shape', _finishMeasurement).on('draw_end', (e) => console.log(e))
+    Map.on('new_shape', _finishMeasurement);
     store.dispatch({ activeId: _id });
     draw.line.enable(MEASURE_LINE);
 };
 
-function _Length ({activeId, ..._props}) {
-    const [totalLength, setLength] = React.useState(0);
-
+function _Length ({activeId, currentMeasure, ..._props}) {
     return <Button {..._props}
         id={_id} 
         className={activeId === _id ? 'active' : ''}
@@ -48,20 +52,30 @@ function _Length ({activeId, ..._props}) {
                         <DrawActions
                             finish={() => draw.line._finishShape()} 
                             undo={() => draw.line._removeLastVertex()}
-                            cancel={_disable} />
+                            cancel={_disable /* implement cancel rather than disable */} />
                     </Modal.Title>
-                    <Modal.Body children={<Button disabled className='as-label' >{totalLength}</Button>} />
+                    <Modal.Body >
+                        <Button 
+                            disabled 
+                            size='lg' 
+                            className='as-label' >
+                                {(_sum + currentMeasure)  + ' m'}
+                        </Button>
+                    </Modal.Body>
                     <Modal.Footer children={<Button onClick={_disable} >Заврши</Button>} />
                 </Modal>}
     </Button>
 }
 
 _Length.propTypes = {
-    options: PropTypes.object,
+    currentMeasure: PropTypes.string,
     activeId: PropTypes.string,
     dispatch: PropTypes.func
 }
 
-export const Length = connect(({process}) => { 
-    return { activeId: process.activeId };
+export const Length = connect(({ process, measurement: {totalLength} }) => {
+    return { 
+        activeId: process.activeId,
+        currentMeasure: calcCurrentLength(totalLength)
+    };
 })(_Length);
