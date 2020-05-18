4b80741e352a6eb35296ba3469e1c50cc9979883
Igi <i.dimov@prtech.mk>
2020-05-18 12:54:02 +0200

    Disable hintline measurement configuration. Simple solution to the implementation of all measurement tools as well as the underlining measurement implementation:  - no performance cost on recalculation  - no creation of an absurd amount of DOM elements (for the labels) each second  - simpler event logic for the UI functionalities.

--- frontend/config/Constants.js
+++ frontend/config/Constants.js
@@ -128,7 +128,9 @@ export const MEASURE_LINE = {
         opacity: 0.9,
         fillOpacity: 0.25,
         dashArray: [10, 10],
-        showMeasurements: true
+        /* This will enable runtime calculation of measurements on each new pixel location of the mouse cursor. */
+        /* DO NOT ENABLE, impedes application performance, creates hundreds - thousands DOM elements each second. */
+        /* showMeasurements: true */ 
     },
     // show a marker at the cursor
     cursorMarker: false,
