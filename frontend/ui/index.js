eec115199323b1d7b9807cc61f5845cfe3926b85
Igi <i.dimov@prtech.mk>
2020-05-18 14:54:21 +0200

    Fixed index for Eraser / eraser exports.

--- frontend/ui/index.js
+++ frontend/ui/index.js
@@ -41,7 +41,7 @@ export { Loading } from './modules/loading/Loading';
 export { Length, length } from './modules/measurement/Length';
 export { Area, areaMeasurements } from './modules/measurement/Area';
 export { Angle, angleMeasurements } from './modules/measurement/Angle';
-export { Eraser, clearLength, clearArea, clearMeasurements } from './modules/measurement/Eraser';
+export { Eraser, eraser } from './modules/measurement/Eraser';
 export { Measurement } from './modules/measurement/Measurement';
 
 export { scale } from './modules/scale/Scale';
