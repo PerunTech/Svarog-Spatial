256a38b3fc4b737f87db50cd65bed15476cad0e0
Igi <i.dimov@prtech.mk>
2020-05-20 08:33:12 +0200

    Added zoom out icon

--- frontend/assets/svg/Registry.js
+++ frontend/assets/svg/Registry.js
@@ -465,6 +465,18 @@ const _registry = {
                 8-8h136c22.089844 0 40-17.910156 40-40s-17.910156-40-40-40zm0 0"/>
         </g>
     },
+    /* <div>Icons made by <a href="https://www.flaticon.com/authors/pixel-perfect" title="Pixel perfect">Pixel perfect</a>
+        from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a></div> */
+    'zoom-out': {
+        version: '1.1',
+        x: '0px',
+        y: '0px',
+        viewBox: '0 -192 469.33333 469',
+        path: <g>
+            <path d="m437.332031.167969h-405.332031c-17.664062 0-32 14.335937-32 32v21.332031c0 17.664062 14.335938
+                32 32 32h405.332031c17.664063 0 32-14.335938 32-32v-21.332031c0-17.664063-14.335937-32-32-32zm0 0"/>
+        </g>
+    },
 };
 
 export const registry = {
