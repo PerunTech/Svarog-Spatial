f941f42dc38199d648cf3b173d52b995d8874603
Igi <i.dimov@prtech.mk>
2020-05-21 13:14:07 +0200

    Remove unused parameter in ToolsBar.jsx

--- frontend/ui/layouts/ToolsBar.jsx
+++ frontend/ui/layouts/ToolsBar.jsx
@@ -2,7 +2,7 @@ import { React } from 'perun-core';
 import { control } from '../../core';
 import { Tab, Tabs, Measurement, Button } from '..';
 
-export const ToolsBar = props => 
+export const ToolsBar = () => 
     <div id='toolsbar' style={{marginLeft: '249px', marginRight: '249px', borderLeft: '1px solid rgba(0, 0, 0, 0.25)'}}>
         <Tabs defaultActiveKey='digitization' >
             <Tab eventKey='digitization' title='Дигитизација' >
