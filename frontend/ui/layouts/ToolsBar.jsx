37bddba785afb7c510929690761de05c81ed6ada
Igi <i.dimov@prtech.mk>
2020-05-21 13:45:12 +0200

    Tweaks to modal.css, ToolsBar.jsx

--- frontend/ui/layouts/ToolsBar.jsx
+++ frontend/ui/layouts/ToolsBar.jsx
@@ -5,10 +5,13 @@ import { Tab, Tabs, Measurement, Button } from '..';
 export const ToolsBar = () => 
     <div id='toolsbar' style={{marginLeft: '249px', marginRight: '249px', borderLeft: '1px solid rgba(0, 0, 0, 0.25)'}}>
         <Tabs defaultActiveKey='digitization' >
-            <Tab eventKey='digitization' title='Дигитизација' >
+            <Tab eventKey='selection' title='Селекција' >
                 <Button size='' disabled className='as-label'>Во Изградба</Button>
             </Tab>
-            <Tab eventKey='selection' title='Селекција' >
+            <Tab eventKey='digitization' title='Напредна Дигитизација' >
+                <Button size='' disabled className='as-label'>Во Изградба</Button>
+            </Tab>
+            <Tab eventKey='io' title='Увоз / Извоз' >
                 <Button size='' disabled className='as-label'>Во Изградба</Button>
             </Tab>
             <Tab eventKey='measurement' title='Мерења' >
