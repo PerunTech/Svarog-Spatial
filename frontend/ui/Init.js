4edbdf8d5ae2ea48ba872bbe60ca070d303db28a
Igi <i.dimov@prtech.mk>
2020-07-28 10:47:05 +0200

    wip frontend/ui/Init.js, added _appBuilder methods for layout / module init. Built js bundle.

--- frontend/ui/Init.js
+++ frontend/ui/Init.js
@@ -1,11 +1,43 @@
 import { React } from 'perun-core';
-import { store, Provider, MapContainer } from '../core';
+import { store, Provider, MapContainer, control, Map } from '../core';
+import { StatusBar, LayerPanel, ToolsBar, NavigationControl, scale } from '../ui';
 
 /**
  * 
  */
 const _appBuilder = {
-    render: () => <Provider children={<MapContainer />} />
+    withStatusBar () {
+        control(StatusBar, {}, {position: 'bottom', className: 'status-bar'});
+        return this;
+    },
+
+    withToolsBar () {
+        control(ToolsBar, {}, {position: 'top'});
+        return this;
+    },
+
+    withLayerPanel () {
+        control(LayerPanel, {}, { position: 'right' });
+        return this;
+    },
+
+    withDataPanel () {
+        return this;
+    },
+
+    withNavigationControl () {
+        control(NavigationControl, {}, {position: 'bottomright'});
+        return this;
+    },
+
+    withWindRose (opt = { metric: true, imperial: false }) {
+        scale(opt).addTo(Map);
+        return this;
+    },
+
+    render () {
+        return <Provider children={<MapContainer />} />;
+    } 
 }
 _appBuilder.render.displayName = 'spatial-root';
 
