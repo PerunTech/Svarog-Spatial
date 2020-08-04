10a78111cd93215033fd553a60faf70b57b42488
Igi <i.dimov@prtech.mk>
2020-08-04 15:54:41 +0200

    Added selection, changed selection-frame in Registry.js Tweaked Selection.js, added selection by polygon
    Built js bundle.

--- frontend/ui/modules/selection/Selection.js
+++ frontend/ui/modules/selection/Selection.js
@@ -12,12 +12,12 @@ export const Selection = () =>
             <span style={{display: 'block', marginTop: '5px' }}>Избери геометрија</span>
         </Button>
         <Button >
-            <Icon name='selection-frame' size='30px' />
-            <span style={{display: 'block', marginTop: '5px' }}>Избери со рамка</span>
-        </Button>
-        <Button >
             <Icon name='selection-polygon' size='30px' />
             <span style={{display: 'block', marginTop: '5px' }}>Избери со полигон</span>
+        </Button>
+        <Button >
+            <Icon name='selection-frame' size='30px' />
+            <span style={{display: 'block', marginTop: '5px' }}>Избери со рамка</span>
         </Button>
         <Button >
             <Icon name='selection-radius' size='30px' />
