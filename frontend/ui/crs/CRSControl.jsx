9ea6a40ef7a74984ea20e391f44c94872aa2b15b
Igi <i.dimov@prtech.mk>
2020-08-05 14:22:40 +0200

    Moved ui/modules/crs/CRSControl.jsx one level up, fixed import paths.

--- frontend/ui/crs/CRSControl.jsx
+++ frontend/ui/crs/CRSControl.jsx
@@ -1,6 +1,6 @@
 import { React } from 'perun-core';
-import { Map } from '../../../core';
-import { Button, Icon } from '../..';
+import { Map } from '../../core';
+import { Button, Icon } from '..';
 
 export const CRSControl = () => 
         <Button style={{width: '200px'}} disabled className='as-label' >
