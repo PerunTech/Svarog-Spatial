b18a62f614669895e4ef6fb734468a448cf36613
Igi <i.dimov@prtech.mk>
2020-08-05 14:10:40 +0200

    Removed layout methods from Init.js.

--- frontend/ui/Init.js
+++ frontend/ui/Init.js
@@ -1,35 +1,11 @@
 import { React } from 'perun-core';
 import { store, Provider, MapContainer, control, Map } from '../core';
-import { StatusBar, LayerPanel, ToolsBar, DataPanel, NavigationControl, scale } from '../ui';
+import { NavigationControl, scale } from '../ui';
 
 /**
  * 
  */
 const _appBuilder = {
-    withStatusBar (props = {}, opt = {}) {
-        control(StatusBar, props, { position: 'bottom', className: 'status-bar', ...opt });
-
-        return this;
-    },
-
-    withToolsBar (props = {}, opt = {}) {
-        control(ToolsBar, props, { position: 'top', ...opt });
-
-        return this;
-    },
-
-    withLayerPanel (props = {}, opt = {}) {
-        control(LayerPanel, props, { position: 'right', ...opt });
-        
-        return this;
-    },
-
-    withDataPanel (props = {}, opt = {}) {
-        control(DataPanel, props, { position: 'left', ...opt });
-
-        return this;
-    },
-
     withNavigationControl (props = {}, opt = {}) {
         control(NavigationControl, props, {position: 'bottomright', ...opt});
         
