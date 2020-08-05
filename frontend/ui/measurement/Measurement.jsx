0a89291ca05221e816ed81e2c1d78648d702a652
Igi <i.dimov@prtech.mk>
2020-08-05 14:29:39 +0200

    Moved ui/modules/measurement/Measurement.jsx one level up, fixed import paths.

--- frontend/ui/measurement/Measurement.jsx
+++ frontend/ui/measurement/Measurement.jsx
@@ -1,5 +1,5 @@
 import { React } from 'perun-core';
-import { ButtonGroup, Length, Area, Angle, Radius, Eraser } from '../..';
+import { ButtonGroup, Length, Area, Angle, Radius, Eraser } from '..';
 
 export const Measurement = () =>
     <ButtonGroup id='measurement' >
