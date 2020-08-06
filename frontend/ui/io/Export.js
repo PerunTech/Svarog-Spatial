b61ad7b0dcdcf343791fab5eac8bb8f45dd6c858
Igi <i.dimov@prtech.mk>
2020-08-06 10:02:18 +0200

    Tweaked Export.js and Import.js labels. Built js bundle

--- frontend/ui/io/Export.js
+++ frontend/ui/io/Export.js
@@ -5,27 +5,27 @@ export const Export = () =>
     <ButtonGroup id='io' >
         <Button >
             <Icon name='shapefile' size='30px' />
-            <span style={{display: 'block', marginTop: '5px' }}>SHP датотека</span>
+            <span style={{display: 'block', marginTop: '5px' }}>Извези SHP</span>
         </Button>
         <Button >
             <Icon name='geojson' size='30px' />
-            <span style={{display: 'block', marginTop: '5px' }}>GeoJSON датотека</span>
+            <span style={{display: 'block', marginTop: '5px' }}>Извези GeoJSON</span>
         </Button>
         <Button >
             <Icon name='csv' size='30px' />
-            <span style={{display: 'block', marginTop: '5px' }}>CSV датотека</span>
+            <span style={{display: 'block', marginTop: '5px' }}>Извези CSV</span>
         </Button>
         <Button >
             <Icon name='xsl' size='30px' />
-            <span style={{display: 'block', marginTop: '5px' }}>XSL датотека</span>
+            <span style={{display: 'block', marginTop: '5px' }}>Извези XSL</span>
         </Button>
         <Button >
             <Icon name='pdf' size='30px' />
-            <span style={{display: 'block', marginTop: '5px' }}>PDF датотека</span>
+            <span style={{display: 'block', marginTop: '5px' }}>Извези PDF</span>
         </Button>
         <Button >
             <Icon name='map-report' size='30px' />
-            <span style={{display: 'block', marginTop: '5px' }}>Картографија</span>
+            <span style={{display: 'block', marginTop: '5px' }}>Извези Картографија</span>
         </Button>
         <Button >
             <Icon name='printer' size='30px' />
