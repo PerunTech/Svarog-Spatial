d9a9966bd2b6bdb8c1a8ba2dc54dad8255e3bf5e
Igi <i.dimov@prtech.mk>
2020-08-18 15:09:08 +0200

    Formed api surface in data/geobuf/Geobuf.js Export include in index.js
    Built js bundle.

--- frontend/data/geobuf/Geobuf.js
+++ frontend/data/geobuf/Geobuf.js
@@ -1,6 +1,12 @@
-
+import { encode as _encode } from './Encode';
+import { decode as _decode } from './Decode';
 
 export const geobuf = {
-    encode () {},
-    decode () {}
+    encode (obj, pbf) {
+        return _encode(obj, pbf);
+    },
+
+    decode (pbf) {
+        return _decode(pbf);
+    }
 }
--- frontend/data/index.js
+++ frontend/data/index.js
@@ -1,1 +1,2 @@
+export { geobuf } from './geobuf/Geobuf';
 export { layerControl, raster } from './raster/Raster';
