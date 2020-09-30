c55b460af6a29557b01a93bd35c0e3d60e9681bd
Igi <i.dimov@prtech.mk>
2020-09-30 12:33:43 +0200

    Added replaceElement in core/utils/Array.js

--- frontend/core/utils/Array.js
+++ frontend/core/utils/Array.js
@@ -66,4 +66,33 @@ export const arr = {
     normalize (obj, type = String) {
         return obj.toString().replace(/\s+/g, '').split(/[;,/]/).map(type);
     },
+
+    /**
+     * Replaces an element in an array for a given index.
+     * 
+     * If the index exceeds the array length, no replacement occurs.
+     * Returns a new array with copied elements.
+     * 
+     * This utility provides a functional approach to basic array modification,
+     * given the shortcoming of the Array.prototype.splice method which returns
+     * the replaced element instead of the resulting array set.
+     * 
+     * &nbsp;
+     * 
+     * @function replaceElement (arr: Array, idx: number, el: any): []
+     * 
+     * @param {Array} arr - The array to be modified. 
+     * @param {number} idx - The index where the modification occurs.
+     * @param {*} el - The element to be inserted.
+     * 
+     * @return [ ...arr, arr[id] = el ];
+     */
+    replaceElement (arr, idx, el) {
+        let result = [ ...arr ];
+
+        idx < result.length
+            && (result[idx] = el);
+
+        return result;
+    }
 };
