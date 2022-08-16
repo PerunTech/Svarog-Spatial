db267b6ba85cbbe15eab951eb3a5016db2375bde
r.pejov <r.pejov@prtech.mk>
2022-08-16 17:01:26 +0200

    fix typo for not null check

--- backend/src/com/prtech/spatial/geobuf/GeobufEncoder.java
+++ backend/src/com/prtech/spatial/geobuf/GeobufEncoder.java
@@ -304,7 +304,7 @@ public class GeobufEncoder {
 				val.setDoubleValue(((Number) featVal).doubleValue());
 			else {
 				// TODO serialize to JSON
-				if (val != null) {
+				if (featVal != null) {
 					if (LOG.isDebugEnabled())
 						LOG.debug("Unable to save object of type " + featVal.getClass().getTypeName()
 								+ " to geobuf, falling back on toString. Deserialization will not work as expected.",
