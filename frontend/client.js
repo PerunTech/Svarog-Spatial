eaca984b69fa0fc949e131cecef622cb66ad02e9
tomi.karikj <t.karikj@prtech.mk>
2024-02-12 15:25:32 +0100

    updated webpack-dev-server to the latset version (5.0.0)
    updated the import of the package.json properties

--- frontend/client.js
+++ frontend/client.js
@@ -1,5 +1,5 @@
 import { pluginManager } from 'perun-core';
-import { name } from '../package.json'
+import pkg from '../package.json'
 import { spatial } from '.';
 
-pluginManager.registerPlugin(name, spatial);
+pluginManager.registerPlugin(pkg.name, spatial);
