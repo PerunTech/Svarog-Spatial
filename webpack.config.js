4a93146a4c2d129f41c75ae44035987e7e1d336e
tomi.karikj <t.karikj@prtech.mk>
2024-09-16 09:16:13 +0200

    properly bundle the images and fonts

--- webpack.config.js
+++ webpack.config.js
@@ -58,10 +58,7 @@ module.exports = (_, { mode }) => {
                 },
                 {
                     test: /\.(png|jpe?g|gif|svg|eot|ttf|woff|woff2)$/i,
-                    loader: 'url-loader',
-                    options: {
-                        limit: 10000,
-                    }
+                    type: 'asset/resource',
                 },
             ]
         },
